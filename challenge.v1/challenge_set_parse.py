e4630bd675bf211692469641a732b2caf7ba2555
Carsen Ball <ballcarsen@gmail.com>
2018-05-03 13:40:52 +0200

    challenge subsets

--- challenge.v1/challenge_set_parse.py
+++ challenge.v1/challenge_set_parse.py
@@ -20,8 +20,8 @@ while count < 9000:
         print(challenge_set["playlists"][count]["name"],challenge_set["playlists"][count]["num_tracks"],challenge_set["playlists"][count]["num_samples"])
         count += 1
 '''
-slice = challenge_set['playlists'][1000:2000]
+slice = challenge_set['playlists'][8000:9000]
 
 count = 1000
-with open('challenge_3.json', 'w') as f:
+with open('challenge_10.json', 'w') as f:
     json.dump(slice, f, indent=2)
